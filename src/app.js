461e41f431b243c08f127686d18df341107f01bf
Tavoide <gustavobenitez116@gmail.com>
2024-11-19 15:46:38 +0000

    generador de dominio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extencion = [".com", ".net", ".us", ".io"];
+
+for (let a = 0; a < pronoun.length; a++) {
+  for (let b = 0; b < adj.length; b++) {
+    for (let c = 0; c < noun.length; c++) {
+      for (let d = 0; d < extencion.length; d++);
+    }
+  }
+  console.log(prom[a] + adj[b] + noun[c] + extencion[d]);
+}
+
+// window.onload = function() {
+//   //write your code here
+
+//   console.log("Hello Rigo from the console!");
+// };
