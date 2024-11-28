cae88ec2674fa911ad1b11afbee81c6272c73bcb
Tavoide <gustavobenitez116@gmail.com>
2024-11-28 02:09:04 +0000

    generador de dominios

--- src/app.js
+++ src/app.js
@@ -4,23 +4,24 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+window.onload = function() {
+  document.querySelector("#dominio").innerHTML = generadorDeDominios();
+};
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
-let extencion = [".com", ".net", ".us", ".io"];
+let extension = [".com", ".es", ".io", ".org"];
 
-for (let a = 0; a < pronoun.length; a++) {
-  for (let b = 0; b < adj.length; b++) {
-    for (let c = 0; c < noun.length; c++) {
-      for (let d = 0; d < extencion.length; d++);
+const generadorDeDominios = () => {
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < extension.length; d++) {
+          console.log(pronoun[a] + adj[b] + noun[c] + extension[d]);
+        }
+      }
     }
   }
-  console.log(prom[a] + adj[b] + noun[c] + extencion[d]);
-}
+};
 
-// window.onload = function() {
-//   //write your code here
-
-//   console.log("Hello Rigo from the console!");
-// };
+generadorDeDominios;
